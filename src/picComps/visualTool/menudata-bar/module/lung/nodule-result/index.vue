5f18910530d7baa9a5d02719d70839d2bf121cc5
fangh <marklow@126.com>
2024-06-25 10:41:53 +0800

    style

--- src/picComps/visualTool/menudata-bar/module/lung/nodule-result/index.vue
+++ src/picComps/visualTool/menudata-bar/module/lung/nodule-result/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="nodule-result_panel">
     <checkToolBar></checkToolBar>
+    <LesionList></LesionList>
   </div>
 </template>
 <script lang='javascript'>
 import checkToolBar from "./module/check-tool-bar.vue";
+import LesionList from "./module/lesion-list.vue";
 // 肺部- 结节
 export default {
   name: "nodule-result",
   components: {
     checkToolBar,
+    LesionList,
   },
   data() {
     return {};
---
+++ src/picComps/visualTool/menudata-bar/module/lung/nodule-result/module/lesion-list.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="nodule_lesion-list">
+    <div class="table_bar flex justify-between">
+      <div class="tit">病变列表</div>
+      <div class="btn_grp flex">
+        <ta-dropdown :trigger="['click']">
+          <a href="javascript:;">
+            {{ sort_condition.type_select.showValue }} <ta-icon type="down" />
+          </a>
+          <ta-menu slot="overlay" @click="handleMenuClick">
+            <ta-menu-item
+              v-for="(item, index) in sort_condition.type_select.list"
+              :key="`${index}_${item.value}`"
+              >{{ item.label }}</ta-menu-item
+            >
+            <!-- <ta-menu-divider /> -->
+          </ta-menu>
+        </ta-dropdown>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang='javascript'>
+// 病变列表
+export default {
+  name: "lesion-list",
+  data() {
+    return {
+      sort_condition: {
+        // 普通类型 （风险~体积~）
+        type_select: {
+          showValue: "按类型", //显示选中值
+          list: [
+            {
+              label: "默认",
+              value: "",
+            },
+            {
+              label: "按风险",
+              value: "0",
+            },
+            {
+              label: "按IM",
+              value: "1",
+            },
+            {
+              label: "按肺段",
+              value: "2",
+            },
+            {
+              label: "按长径",
+              value: "3",
+            },
+            {
+              label: "按体积",
+              value: "4",
+            },
+            {
+              label: "按类型",
+              value: "5",
+            },
+          ],
+        },
+        // 肿瘤信息类型
+        tumorType_select: {
+          showValue: "筛选",
+          searchPanel: [
+            {
+              title: "良恶性",
+              list: [
+                {
+                  label: "全部",
+                  value: "",
+                },
+                {
+                  label: "低危",
+                  value: "0",
+                },
+                {
+                  label: "中危",
+                  value: "1",
+                },
+                {
+                  label: "高危",
+                  value: "2",
+                },
+              ],
+            },
+            {
+              title: "类型",
+              list: [
+                {
+                  label: "全部",
+                  value: "",
+                },
+                {
+                  label: "肿块",
+                  value: "0",
+                },
+                {
+                  label: "混合",
+                  value: "1",
+                },
+                {
+                  label: "磨玻璃",
+                  value: "2",
+                },
+                {
+                  label: "实性",
+                  value: "3",
+                },
+                {
+                  label: "钙化",
+                  value: "4",
+                },
+              ],
+            },
+            {
+              title: "长径",
+              list: [
+                {
+                  label: "全部",
+                  value: "",
+                },
+                {
+                  label: "0-3mm",
+                  value: "",
+                },
+                {
+                  label: "3-5mm",
+                  value: "",
+                },
+                {
+                  label: "5-8mm",
+                  value: "",
+                },
+                {
+                  label: ">8mm",
+                  value: "",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
+  },
+  methods: {
+    handleMenuClick(e) {
+      console.log("click:handleMenuClick:", e);
+      const { key } = e;
+      const keyFindArr = key.split("_");
+      console.log("keyFindArr=", keyFindArr);
+      const idx = keyFindArr[0];
+
+      const row = this.sort_condition.type_select.list[idx];
+      console.log("sort_condition.type_select==row", row);
+      this.sort_condition.type_select.showValue = row.label;
+    },
+  },
+};
+</script>
+<style lang='less' scoped>
+</style>
