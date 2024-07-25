8ceb5c9497edaa33b5f064c1d32b80d103fc14ee
fangh <marklow@126.com>
2024-07-25 16:31:47 +0800

    mids

--- src/assets/js/utils/url-toto.ts
+++ src/assets/js/utils/url-toto.ts
@@ -5,7 +5,8 @@ export const getBaseURL = () => {
   if (isProduction) {
     // 生产环境使用location.origin加上VITE_LOGIN_SYS_BASE_SORT的环境变量
     // baseURL = `${location.origin}${process.env.VITE_LOGIN_SYS_BASE_SORT}`;
-    baseURL = `${process.env.VITE_LOGIN_SYS_BASE_SORT}`;
+    // baseURL = `${process.env.VITE_LOGIN_SYS_BASE_SORT}`;
+    baseURL = `/mids`;
   } else {
     // 非生产环境使用location.origin
     baseURL = location.origin;
