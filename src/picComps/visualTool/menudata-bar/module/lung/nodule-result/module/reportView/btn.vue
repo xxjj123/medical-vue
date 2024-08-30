6c22f190cbcbcb20ed4be308ad43d87fb005df4f
fangh <marklow@126.com>
2024-08-30 12:02:43 +0800

    report

---
+++ src/picComps/visualTool/menudata-bar/module/lung/nodule-result/module/reportView/btn.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="report_btn" style="width:100%">
+    <ta-button type="primary" size="large" style="width:100%">报告</ta-button>
+
+
+  </div>
+</template>
+<script lang='javascript'>
+export default {
+  name: 'reportViewBtn',
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang='less' scoped></style>
